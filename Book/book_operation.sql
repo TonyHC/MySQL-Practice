2ff35ff1ba082b8465406447ad5cc31ee7a28841
Tony Chao <tchao@mail.usf.edu>
2021-08-27 21:15:17 -0400

    Book DB CRUD Operation

---
+++ Book/book_operation.sql
@@ -0,0 +1,20 @@
+# SELECT 
+#     CONCAT
+#     (
+#         SUBSTRING(title, 1, 10), 
+#         '...'
+#     ) AS 'Book Title' 
+# FROM books;
+
+# SELECT SUBSTRING(REPLACE(title, 'The', 'A'), 1, 10) AS 'New Title' FROM books;
+
+# SELECT
+#   CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title',
+#   CONCAT_WS(',', author_lname, author_fname) AS 'author',
+#   CONCAT_WS(' ', stock_quantity, 'in stock') AS 'quantity'
+# FROM books;
+
+# SELECT author_lname, author_fname FROM books ORDER BY author_lname, author_fname;
+# SELECT UPPER(CONCAT('my favorite author is', ' ', author_fname, ' ', author_lname, '!')) AS 'yell' FROM books ORDER BY author_lname;
+
+INSERT INTO people(name, birthdate, birthtime, birthdt) VALUES ('Thomas', CURDATE(), CURTIME(), NOW());
