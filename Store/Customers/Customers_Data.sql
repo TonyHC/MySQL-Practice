24cc8baab69727f83faf3d9ab59d8a9208830bb6
Tony Chao <tchao@mail.usf.edu>
2021-08-29 14:01:01 -0400

    Customers Table Data

---
+++ Store/Customers/Customers_Data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
