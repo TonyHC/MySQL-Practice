81c61389341793f29d19f25cd4421fd903113603
Tony Chao <tchao@mail.usf.edu>
2021-08-29 15:17:12 -0400

    Papers Table Data

---
+++ Classroom/Papers/Papers_Data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Papers (student_id, title, grade ) VALUES
+(1, 'My First Book Report', 60),
+(1, 'My Second Book Report', 75),
+(2, 'Russian Lit Through The Ages', 94),
+(2, 'De Montaigne and The Art of The Essay', 98),
+(4, 'Borges and Magical Realism', 89);
